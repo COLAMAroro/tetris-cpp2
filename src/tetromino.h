8dc88907e2da5c9ade78049641f4d2294d7cfd00
COLAMAroro <github@rondier.io>
2023-04-13 22:59:44 +0200

    Converting indentation to tabs (and align Makefile ident to 8, matching GitHub)
    See: https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/

--- src/tetromino.h
+++ src/tetromino.h
@@ -4,21 +4,21 @@
 class Tetromino
 {
 public:
-    Tetromino();
-    void updateBoard();
-    bool moveRight();
-    bool moveLeft();
-    bool moveDown();
-    bool moveUp();
-    bool rotate(bool=false);
-    int board[24][10] = {};
+	Tetromino();
+	void updateBoard();
+	bool moveRight();
+	bool moveLeft();
+	bool moveDown();
+	bool moveUp();
+	bool rotate(bool=false);
+	int board[24][10] = {};
 
 private:
-    int y = 1;
-    int x = 3;
-    int rotation;
-    int type;
-    bool collideWithBorder();
+	int y = 1;
+	int x = 3;
+	int rotation;
+	int type;
+	bool collideWithBorder();
 };
 
 #endif
